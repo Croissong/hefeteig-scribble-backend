5d527fb40237e1ebdf3ecccff82f28c52dd5dc88
Semih Helvaci <semih.helvaci92@gmail.com>
2019-11-24 20:44:56 +0100

    Add subscription to messages

--- src/main/kotlin/io/hefeteig/hefeteigscribblebackend/ChatService.kt
+++ src/main/kotlin/io/hefeteig/hefeteigscribblebackend/ChatService.kt
@@ -4,32 +4,50 @@ import io.leangen.graphql.annotations.GraphQLArgument
 import io.leangen.graphql.annotations.GraphQLMutation
 import io.leangen.graphql.annotations.GraphQLQuery
 import io.leangen.graphql.annotations.GraphQLSubscription
-import io.leangen.graphql.spqr.spring.annotations.GraphQLApi
-import org.springframework.stereotype.Component
+import io.leangen.graphql.spqr.spring.annotations.GraphQLApi;
+import io.leangen.graphql.spqr.spring.util.ConcurrentMultiMap
+import org.reactivestreams.Publisher
+import org.springframework.context.annotation.Bean
+import org.springframework.scheduling.TaskScheduler
+import org.springframework.scheduling.annotation.EnableScheduling
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler
+import org.springframework.stereotype.Service
 import reactor.core.publisher.Flux
 import reactor.core.publisher.FluxSink
+
 import java.time.Instant
 
-@Component
+@Service
 @GraphQLApi
+@EnableScheduling
 class ChatService {
-    var subscribers: MutableMap<String, FluxSink<Any>> = mutableMapOf()
-    var messages: MutableList<Message> = mutableListOf(Message("InitialUser1", "InitialText1", Instant.now().toEpochMilli()), Message("InitialUser1", "InitialText2", Instant.now().toEpochMilli()+5000))
+    val subscribers: ConcurrentMultiMap<String, FluxSink<Message>> = ConcurrentMultiMap()
 
-    @GraphQLQuery
-    fun messages() : List<Message>  = this.messages
+    @Scheduled(fixedRate = 5000)
+    fun testMessage() {
+        var time = Instant.now().toEpochMilli()
+        subscribers.get("test").forEach { subscriber -> subscriber.next(Message("TestUser", "TestMessage $time, subscribers: ${subscribers.get("test").size}", time))}
+    }
 
     @GraphQLMutation
     fun sendMessage(@GraphQLArgument(name = "username", defaultValue = "") username: String, @GraphQLArgument(name = "message", defaultValue = "") message: String): Message {
         val m = Message(username, message, Instant.now().toEpochMilli())
-        println(m)
-        this.messages.add(m)
+        this.subscribers.get(username).forEach {subscriber -> subscriber.next(m)}
         return m
     }
 
     @GraphQLSubscription
-    fun sub(username: String): Flux<Any> {
-        println("sub : $username");
-        return Flux.create<Any>({ subscriber -> this.subscribers[username] = subscriber.onDispose { this.subscribers.remove(username, subscriber) } }, FluxSink.OverflowStrategy.LATEST)
+    fun messagesSubscription(username: String): Publisher<Message> {
+        return Flux.create({ subscriber -> this.subscribers.add(username, subscriber.onDispose { this.subscribers.remove(username, subscriber) }) }, FluxSink.OverflowStrategy.LATEST)
+    }
+
+    @Bean
+    fun taskScheduler(): TaskScheduler {
+        val scheduler = ThreadPoolTaskScheduler()
+        scheduler.poolSize = 2
+        scheduler.setThreadNamePrefix("scheduled-task-")
+        scheduler.isDaemon = true
+        return scheduler
     }
 }
--- src/main/kotlin/io/hefeteig/hefeteigscribblebackend/HefeteigScribbleBackendApplication.kt
+++ src/main/kotlin/io/hefeteig/hefeteigscribblebackend/HefeteigScribbleBackendApplication.kt
@@ -2,6 +2,13 @@ package io.hefeteig.hefeteigscribblebackend
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.scheduling.annotation.EnableScheduling
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler
+import org.springframework.scheduling.TaskScheduler
+
+
+
 
 @SpringBootApplication
 class HefeteigScribbleBackendApplication
